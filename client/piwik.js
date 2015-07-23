fa94df3a1b7cdf1ea35daf362d8e3d0fa0a0f57f
Ohad Winnykamien <ohad@masheen.io>
2015-07-23 16:42:12 +0300

    -bug fix on event tracking -added user id option -trackThisPage function -remove deprecated and unused application-configuration dependency

--- client/piwik.js
+++ client/piwik.js
@@ -1,12 +1,23 @@
 Meteor.Piwik = {
-	trackPage: function(route){
-		var url = Meteor.absoluteUrl(route);
-		url = url.replace(/([^:]\/)\/+/g, "$1"); // replace double forward slashes: "//"
-		Meteor.call("trackPage", url);
-	},
-	trackEvent: function(route, event, options){
-		var url = Meteor.absoluteUrl(route);
-		url = url.replace(/([^:]\/)\/+/g, "$1"); // replace double forward slashes: "//"
-		Meteor.call("trackEvent", url, event, options);
-	}
+  session_uid: null,
+  setUserId: function (uid) {
+    Meteor.Piwik.session_uid = uid;
+  },
+  clearUserId: function (uid) {
+    Meteor.Piwik.session_uid = null;
+  },
+  trackThisPage: function () {
+    Meteor.Piwik.trackPage(Router.current().route.path());
+  },
+  trackPage: function (route) {
+    var url = Meteor.absoluteUrl(route);
+    url = url.replace(/([^:]\/)\/+/g, "$1"); // replace double forward slashes: "//"
+    Meteor.call("trackPage", url, Meteor.Piwik.session_uid);
+  },
+  trackEvent: function (route, event, options) {
+    if (typeof options == 'undefined') options = {};
+    var url = Meteor.absoluteUrl(route);
+    url = url.replace(/([^:]\/)\/+/g, "$1"); // replace double forward slashes: "//"
+    Meteor.call("trackEvent", url, event, options, Meteor.Piwik.session_uid);
+  }
 };
